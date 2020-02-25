8a36fe3e3bfeec2c1410a63365aa046c423caf28
asadlib11 <asadalibaloch059@gmail.com>
2020-02-25 17:30:42 +0500

    =first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This repo is an implementation of builder pattern in Nodejs
---
+++ app.ts
@@ -0,0 +1,2 @@
+import {HouseBuilder} from './house-builder';
+const myHouse = new HouseBuilder("DHA ph 5").setFloor(4).makeParking().makeGarden().build;
